1387f949950dc4a2d54b6a386f16630c3e737778
Penelope Yong <penelopeysm@gmail.com>
2025-05-25 03:53:15 +0100

    Add deployment script

---
+++ build_gh.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+OWNER=penelopeysm
+REPO=london_classical
+GIT_DIR="$(git rev-parse --show-toplevel)"
+FILENAME="concerts.json"
+
+cd "${GIT_DIR}/src/assets"
+gh release upload json ${FILENAME} --clobber
+
+cd ${GIT_DIR}
+WORKFLOW=$(ls .github/workflows)
+gh workflow run "${WORKFLOW}" --ref main
